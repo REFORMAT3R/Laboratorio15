b59eb2e7be217db4b6c834c3b07b8be1d6a0e5fa
REFORMAT3R <dmamanihuayh@unsa.edu.pe>
2025-11-19 20:47:55 -0500

    Agregando más ejercicios

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ console.log(subtitulo.textContent);
 
 console.log("EJERCICIO 02");
 const parrafos = document.querySelectorAll("p");
+let c = 0;
 parrafos.forEach(p => {
-    p.textContent = "Texto actualizado"
-    console.log(p.textContent);
+    if(c < 3){
+        p.textContent = "Texto actualizado";
+        console.log(p.textContent);
+    }
+    c++;
 });
 
 console.log("EJERCICIO 03");
@@ -49,3 +53,45 @@ document.body.appendChild(nuevoParrafo);
 console.log("Nuevo parrafo insertado");
 
 console.log("EJERCICIO 08");
+const nuevoSubtitulo = document.createElement("h3");
+nuevoSubtitulo.textContent = "Subtitulo insertado con JS";
+document.querySelector("section").insertBefore(nuevoSubtitulo, document.querySelector("section").firstElementChild);
+console.log("Nuevo subtitulo insertado");
+
+console.log("EJERCICIO 09");
+const section1 = document.querySelector("section");
+const ul = document.createElement("ul");
+let t = 1;
+while(t<= 5) {
+    const li = document.createElement("li");
+    li.textContent = "Elemento " + t;
+    ul.appendChild(li);
+    t++;
+}
+section1.appendChild(ul);
+console.log(ul);
+
+console.log("EJERCICIO 10");
+const parrafo3 = document.getElementById("parrafoNuevo");
+const div1 = document.createElement("div");
+div1.textContent = "Parrafo reemplazado";
+parrafo3.parentNode.replaceChild(div1, parrafo3)
+console.log(div1.textContent);
+
+console.log("EJERCICIO 11");
+const parrafo4 = document.querySelectorAll("p");
+const ultimoParrafo = parrafo4[parrafo4.length -1];
+ultimoParrafo.parentNode.removeChild(ultimoParrafo);
+console.log(ultimoParrafo.textContent); //Muestra el texto eliminado
+
+console.log("EJERCICIO 12");
+const div2 = document.getElementById("contenido");
+div2.textContent = "";
+console.log(div2.textContent);
+
+console.log("EJERCICIO 13");
+const articulo = document.querySelector("article");
+const clon = articulo.cloneNode(true);
+const main = document.querySelector("main");
+main.appendChild(clon);
+console.log("Clon agregado");
